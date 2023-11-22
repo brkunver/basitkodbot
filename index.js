780b78b2ae95f9ad2879c9e2d53a45f18da2a4d4
Burak <burakhanunver@gmail.com>
2023-11-22 15:41:15 +0300

    yorum eklendi

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
-const TelegramBot = require("node-telegram-bot-api")
-require("dotenv").config()
+const TelegramBot = require("node-telegram-bot-api") // bot kütüphanesi
+require("dotenv").config() // ortam değişkenlerini çağırdım
 
-const token = process.env.token
+const token = process.env.token // token değişkenimi aldım koda
 
-const bot = new TelegramBot(token, { polling: true })
+const bot = new TelegramBot(token, { polling: true }) // yeni bir bot oluşturdum
 
+//bot bir mesaj aldığında
 bot.on("message", (msg) => {
+  
+  // gelen mesajı gönderen grubun id'si
   const id = msg.chat.id
+
+  // eğer benim grupla aynıysa
   if (id == process.env.grup_id) {
+    
+    // ve mesaj saat kaç diyorsa
     if (msg.text == "saat kaç") {
+     
+      // şuanki zamanı al
       let saat = new Date().getHours()
+      
+      // o gruba gönder
       bot.sendMessage(id, `saat şuan : ${saat}`)
     }
   }
